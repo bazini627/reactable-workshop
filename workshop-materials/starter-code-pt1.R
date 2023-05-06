794c440cd4117bf276040e46f326b8d90b0415f4
Matt <mjb@pc-tumbleweed.attlocal.net>
2023-05-06 15:44:53 -0700

    Finished file for spotify table

--- workshop-materials/starter-code-pt1.R
+++ workshop-materials/starter-code-pt1.R
@@ -1,20 +1,19 @@
 # IMPORT LIBRARIES
-#to install packages you can either use the UI (Packages ->Install)
-#or you can install with install.packages("packagename")
-#note, one library is not on CRAN - remotes::install_github("timelyportfolio/dataui")
+# To install packages you can either use the UI (Packages ->Install)
+# Or you can install with install.packages("packagename")
+# Note, one library is not on CRAN - remotes::install_github("timelyportfolio/dataui")
 
-#to build our tables
+# To build our tables
 library(reactablefmtr)
 library(reactable)
-#to add in HTML
+# To add in HTML
 library(htmltools)
 library(htmlwidgets)
-#for added data manipulation
+# For added data manipulation
 library(tidyverse)
 library(glue)
-#save as png 
+# Save as png 
 library(webshot2)
-
 
 #IMPORT HELPER FUNCTIONS - used for additional HTML/CSS styling
 source("https://raw.githubusercontent.com/tashapiro/reactable-workshop/main/workshop-materials/helper_functions.R")
@@ -27,6 +26,11 @@ artist_data<- read.csv("https://raw.githubusercontent.com/tashapiro/reactable-wo
 #create rank with row_number
 #arrange the columns in this order using select: rank, name, genres, followers, populatiy, image_url
 #get the first 500 records
+spotify_artists <- artist_data %>% 
+  arrange(desc(followers)) %>% 
+  mutate(rank = row_number()) %>% 
+  select(rank, name, genres, followers, popularity, image_url) %>% 
+  head(500)
 
 #STORE AESTHETICS IN VARIABLES - helps us stay consistent, easier to remember / edit
 heatmap_pal<-rev(c("#64d591","#afe88d","#D6E888","#fce782","#fdf2c0","#fdfdfd"))
@@ -35,16 +39,79 @@ border_color<-'#2E2E2E'
 
 #Build Spotify Table
 #use reactable, reactablefmtr, and htmltools to customize table
-
+spotify_table <- reactable(spotify_artists,
+          searchable = TRUE,
+          theme = reactableTheme(
+            backgroundColor = bg_color,
+            borderColor = border_color,
+            style = list(
+              color = "white"),
+            # Search Styling
+            searchInputStyle = list(
+              backgroundColor = bg_color,
+              borderColor = border_color
+            )
+            ),
+          defaultColDef = colDef(vAlign = "center"),
+          columns = list(
+            rank = colDef(name = "Rank", align = "center"),
+            image_url = colDef(show = FALSE),
+            name = colDef(name = "Artist", width = 300, 
+                          cell = function(value, index) {
+                            image = spotify_artists$image_url[index]
+                            image_label(image_url = image, 
+                                        label = value, 
+                                        height = 30,
+                                        circle = TRUE,
+                                        border_color = "white",
+                                        border_width = 2
+                                        )
+            }),
+            followers = colDef(name = "Followers", cell = data_bars(spotify_artists,
+                                                                    fill_color = "#1DB954",
+                                                                    text_position = "above",
+                                                                    background = NULL,
+                                                                    text_color = "white",
+                                                                    round_edges = TRUE,
+                                                                    bar_height = 10,
+                                                                    number_fmt = scales::label_number(scale_cut = scales::cut_short_scale()))
+            ),
+            genres = colDef(name = "Genres", align = "left", width = 200,
+                            
+                            cell = function(value) {
+              genre_tags(value)
+            }),
+            popularity = colDef(name = "Popularity", cell = icon_sets(spotify_artists,
+                                                            icons = c("star"),
+                                                            colors = heatmap_pal),
+                                                     footer = "Calulated By Spotify",
+                                                     footerStyle = list(fontSize = 11))
+            ))%>% 
+  reactablefmtr::google_font(font_family = "Heebo")
+  
 
 #Add Header & Caption with htmlwidgets + htmltools
+spotify_finished <- spotify_table%>%
+  htmlwidgets::prependContent(
+    tags$div(style = 'background-color:#0B0B0A;color:white;padding-left:30px;padding-top:5px;',
+             tags$h1("Popular Artists on Spotify",style = "margin-bottom:0px;"),
+             tags$span("Ranking based on follower counts on Spotify. Data as of April 2023.",
+                       style = 'color:#DDDDDD')
+    )
+  )%>%
+  htmlwidgets::appendContent(
+    tags$div("Source: Spotify API {spotifyR}", style = 'background-color:#0B0B0A;color:#DDDDDD;padding-left:30px;padding-bottom:20px;font-size:12px;',
+    )
+  )
 
 
 #preview table output
 
 
 #export as HTML widget with htmlwidget
+htmlwidgets::saveWidget(spotify_finished, file = "spotify.html", selfcontained = TRUE)
 
 
 #export as png with webshot2
+webshot2::webshot("spotify.html", file = "spotify.png")
 
